d1e921d236ea8b3129dc78b8cc8fa03cefc64205
raeyoung <raeyoung233@gmail.com>
2024-12-15 22:06:18 +0900

    test: 포인트 조회 및 이용내역 조회 controller, service test 작성

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,22 +23,64 @@ class PointControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    PointService pointService;
+
     @Test
     @DisplayName("특정 유저의 포인트를 조회한다.")
     public void getUserPointTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/point/0")) // HTTP 요청을 생성 및 설정
+        // given
+        long userId = 1L;
+
+        // when
+        UserPoint userPoint = new UserPoint(userId, 0, System.currentTimeMillis());
+        when(pointService.getPoint(userId)).thenReturn(userPoint);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/point/{userId}", userId)) // HTTP 요청을 생성 및 설정
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(0))
-                .andExpect(jsonPath("$.point").value(0))
-                .andExpect(jsonPath("$.updateMillis").value(0));
+                .andExpect(jsonPath("$.id").exists())                           // 검증
+                .andExpect(jsonPath("$.point").exists())
+                .andExpect(jsonPath("$.updateMillis").exists());
+
+        // verify
+        verify(pointService).getPoint(userId);
     }
 
     @Test
     @DisplayName("특정 유저의 포인트 충전/이용 내역을 조회한다.")
     public void getUserPointHistoryTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/point/1/histories")
-                        .accept(MediaType.APPLICATION_JSON))    // GET 요청에 대한 Accept 헤더를 설정 (JSON 형식의 응답을 기대하는 것)
-                .andExpect(jsonPath("$.length()").value(0));
+        // given
+        long userId = 1L;
+        long id1 = 1L;
+        long id2 = 2L;
+
+        // when
+        List<PointHistory> historyList = List.of(
+                new PointHistory(id1, userId, 1000, TransactionType.CHARGE, System.currentTimeMillis()),
+                new PointHistory(id2, userId, 1000, TransactionType.USE, System.currentTimeMillis())
+        );
+
+        when(pointService.getHistory(userId)).thenReturn(historyList);
+
+        mockMvc.perform(get("/point/{userId}/histories", userId))   // HTTP 요청을 생성 및 설정
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())                 // 검증
+                .andExpect(jsonPath("$[0]").exists())
+                .andExpect(jsonPath("$[1]").exists())
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].userId").exists())
+                .andExpect(jsonPath("$[0].amount").exists())
+                .andExpect(jsonPath("$[0].type").exists())
+                .andExpect(jsonPath("$[0].updateMillis").exists())
+                .andExpect(jsonPath("$[1].id").exists())
+                .andExpect(jsonPath("$[1].userId").exists())
+                .andExpect(jsonPath("$[1].amount").exists())
+                .andExpect(jsonPath("$[1].type").exists())
+                .andExpect(jsonPath("$[1].updateMillis").exists())
+                .andDo(print());
+
+        // verify
+        verify(pointService).getHistory(userId);
     }
 
     @Test
---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,88 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.point.repository.PointRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class PointServiceTest {
+
+    @Mock
+    private PointRepository pointRepository;
+
+    @InjectMocks
+    private PointService pointService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("특정 유저의 포인트를 조회할 경우 성공한다.")
+    public void getPointByUserIdSuccessTest() {
+        // given
+        long userId = -1L;
+        long point = 100L;
+
+        // when
+        UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+        when(pointRepository.selectById(userId)).thenReturn(userPoint);
+
+        UserPoint thenUserPoint = pointService.getPoint(userId);
+
+        // then
+        assertEquals(thenUserPoint.id(), userPoint.id());
+        assertEquals(thenUserPoint.point(), userPoint.point());
+        assertEquals(thenUserPoint.updateMillis(), userPoint.updateMillis());
+
+        // verify
+        verify(pointRepository).selectById(userId);
+    }
+
+    @Test
+    @DisplayName("특정 유저의 포인트 충전/이용 내역을 조회할 경우 성공한다.")
+    public void getUserPointHistorySuccessTest() {
+        // given
+        long userId = 1L;
+        long id1 = 1L;
+        long id2 = 2L;
+
+        // when (예상값)
+        List<PointHistory> historyList = List.of(
+                new PointHistory(id1, userId, 1000, TransactionType.CHARGE, System.currentTimeMillis()),
+                new PointHistory(id2, userId, 1000, TransactionType.USE, System.currentTimeMillis())
+        );
+
+        when(pointRepository.selectAllByUserId(userId)).thenReturn(historyList);
+
+        List<PointHistory> result = pointService.getHistory(userId);
+
+        // then
+        assertNotNull(result);                              // 결과값 null 여부 확인
+        assertEquals(historyList.size(), result.size());    // 결과값 크기가 기대값과 같은지 확인
+        for (int i = 0; i < historyList.size(); i++) {      // historyList 와 result 일치 여부 확인
+            PointHistory expected = historyList.get(i);
+            PointHistory actual = result.get(i);
+
+            assertEquals(expected.id(), actual.id());
+            assertEquals(expected.userId(), actual.userId());
+            assertEquals(expected.amount(), actual.amount());
+            assertEquals(expected.type(), actual.type());
+            assertEquals(expected.updateMillis(), actual.updateMillis());
+        }
+
+        // verify
+        verify(pointRepository).selectAllByUserId(userId);        // pointRepository가 정확히 한 번 호출되었는지 검증
+    }
+}
